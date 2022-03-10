69dd1399c14ad7f55831180fd619abda0123a5b7
onyx6838 <giang.mink@gmail.com>
2022-03-11 00:01:04 +0700

    up

--- src/main/java/com/fam/service/impl/SanPhamService.java
+++ src/main/java/com/fam/service/impl/SanPhamService.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class SanPhamService implements ISanPhamService {
